551dd14dc3d6e6252a3320f38b4df675ce70a312
Richard Jameson <jamesonrichard@hotmail.com>
2023-01-07 18:31:39 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+const alert = document.createElement('ion-alert');
+
+
+let totalExpenses = 0;
+
+const clear = () => {
+	reasonInput.value = '';
+	amountInput.value = '';
+
+};
+
+confirmBtn.addEventListener('click', () => {
+	const enteredReason = reasonInput.value;
+	const enteredAmount = amountInput.value;
+
+	if (enteredReason.trim().length <=0 || 
+		enteredAmount <= 0 || 
+		enteredAmount.trim().length <=0
+	){
+		 presentAlert();
+
+	return;
+}
+const newItem = document.createElement('ion-item');
+newItem.textContent = enteredReason + ': €' + enteredAmount;
+expensesList.appendChild(newItem);
+
+totalExpenses += +enteredAmount;
+totalExpensesOutput.textContent = totalExpenses;
+clear();
+});
+
+cancelBtn.addEventListener('click', clear);
+
+
+  async function presentAlert() {
+    alert.header = 'Invalid input';
+    alert.message = 'Please enter valid input';
+    alert.buttons = ['OK'];
+
+    document.body.appendChild(alert);
+    await alert.present();
+  }
